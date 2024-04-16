f312683e86629dfc588230bcea4a3e90044d2d09
kgr-1019 <k023g0009@g.neec.ac.jp>
2024-04-16 14:03:03 +0900

    03_01_確認課題

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -14,13 +14,47 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
+
+	// シングルインスタンスを取得する
+	input_ = Input::GetInstance();
 }
 
 
 void Player::Update() {
 
+	// スケーリング行列の作成
+
 	// 行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
+
+	// キャラクターの移動ベクトル
+	Vector3 move = {0, 0, 0};
+	// キャラクターの移動速さ
+	const float kCharacterSpeed = 0.2f;
+	// 押した方向で移動ベクトルを変更（左右）
+	if (input_->PushKey(DIK_LEFT)) {
+
+		move.x -= kCharacterSpeed;
+
+	} else if (input_->PushKey(DIK_RIGHT)) {
+
+		move.x += kCharacterSpeed;
+	}
+	// 押した方向で移動ベクトルを変更（上下）
+	if (input_->PushKey(DIK_UP)) {
+
+		move.y -= kCharacterSpeed;
+
+	} else if (input_->PushKey(DIK_DOWN)) {
+
+		move.y += kCharacterSpeed;
+	}
+
+	// 座標移動（ベクトルの加算）
+	Vector3 Add(const WorldTransform& worldTransform_, const Vector3& move) {
+	
+		worldTransform_.translation_ += move;
+	}
 }
 
 
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,6 +1,8 @@
 #pragma once
 #include"Model.h"
 #include"WorldTransform.h"
+#include"Input.h"
+#include"Vector3.h"
 class Player {
 
 public:// メンバ関数
@@ -13,6 +15,7 @@ public:// メンバ関数
 	/// 初期化
 	void Initialize(Model* model, uint32_t textureHandle);
 	
+	Vector3 Add(const WorldTransform& worldTransform_, const Vector3& move);
 
 	/// 更新
 	void Update();
@@ -27,4 +30,9 @@ private:// メンバ変数
 
 	// テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
+
+	// キーボード入力
+	Input* input_ = nullptr;
+
+
 };
